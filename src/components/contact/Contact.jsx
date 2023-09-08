2358483055f5ffb93678f8a4a7e7e7eafe3d468c
Hossain Sany <hossainsany3@gmail.com>
2023-09-08 09:23:00 +0600

    form test

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,29 @@
+import { useHistory } from 'react-router-dom';
 import style from './Contact.module.scss';
 
 const Contact = () => {
+    const history = useHistory();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+
+        fetch('/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: new URLSearchParams(formData).toString(),
+        })
+            .then(() => history.push('/form-success'))
+            .catch((error) => alert(error));
+    };
+
     return (
         <section className={style.contact} id='contact'>
             <div className='container'>
                 <h2 className='section-title'>Contact</h2>
-                <form name='contact' method='POST' action='/form-success' data-netlify='true' netlify-honeypot='bot-field' className={style.form}>
+                <form name='contact' method='POST' onSubmit={handleSubmit} data-netlify='true' netlify-honeypot='bot-field' className={style.form}>
                     <input type='text' placeholder='Name' name='name' required />
                     <input type='email' placeholder='Email' name='email' required />
                     <textarea placeholder='Message' name='message' required />
