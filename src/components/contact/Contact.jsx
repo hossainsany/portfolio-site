8cedfdafee7a9bc78148b344072a6b435c056623
Hossain Sany <hossainsany3@gmail.com>
2023-09-09 01:10:35 +0600

    form validation and handeling

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,37 +1,78 @@
-import { useNavigate } from 'react-router-dom';
 import style from './Contact.module.scss';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useNavigate } from 'react-router-dom';
 
 const Contact = () => {
     const nav = useNavigate();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: '',
+        },
+        // form validation
+        validationSchema: Yup.object({
+            name: Yup.string().max(20, 'Name must me less than 20 characters long.').required('Name is required.'),
+            email: Yup.string().email('Invalid email address.').required('Email is required.'),
+            message: Yup.string().min(20, 'Message must be more than 20 characters long.').required('Message is required.'),
+        }),
 
-        const myForm = event.target;
-        const formData = new FormData(myForm);
-
-        fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: new URLSearchParams(formData).toString(),
-        })
-            .then(() => nav('/form-success'))
-            .catch((error) => {
-                nav('/not-found');
-                console.log(error);
+        // submit form
+        onSubmit: async (values) => {
+            const body = new FormData();
+            Object.entries(values).forEach(([key, val]) => {
+                body.append(key, val);
             });
-    };
+
+            await fetch(import.meta.env.VITE_CONTACT_URI, {
+                method: 'POST',
+                headers: { accept: 'application/json' },
+                body,
+            })
+                .then(() => nav('/form-success'))
+                .catch((err) => console.log(err));
+        },
+    });
 
     return (
         <section className={style.contact} id='contact'>
             <div className='container'>
                 <h2 className='section-title'>Contact</h2>
-                <form name='contact' method='POST' data-netlify='true' onSubmit={handleSubmit} className={style.form}>
-                    <input type='hidden' name='contact' value='contact' />
+                <form onSubmit={formik.handleSubmit} className={style.form}>
+                    <input
+                        type='text'
+                        id='name'
+                        placeholder='Name'
+                        name='name'
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.name && formik.errors.name ? <label htmlFor='name'>*{formik.errors.name}</label> : ''}
 
-                    <input type='text' placeholder='Name' name='name' required />
-                    <input type='email' placeholder='Email' name='email' required />
-                    <textarea placeholder='Message' name='message' required />
+                    <input
+                        type='email'
+                        id='email'
+                        placeholder='Email'
+                        name='email'
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.email && formik.errors.email ? <label htmlFor='email'>*{formik.errors.email}</label> : ''}
+
+                    <textarea
+                        id='message'
+                        placeholder='Message'
+                        name='message'
+                        value={formik.values.message}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.message && formik.errors.message ? <label htmlFor='message'>*{formik.errors.message}</label> : ''}
+
                     <button type='submit' className={`btn ${style.submitBtn}`}>
                         Submit
                     </button>
