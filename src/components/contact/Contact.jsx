8a0a1db657fb5e53934c39a799107b141f6f502c
Hossain Sany <hossainsany3@gmail.com>
2023-09-08 10:09:18 +0600

    Revert "netlify form"
    This reverts commit 5fa9a32410a0aa7f314823354021b3a5ba89de83.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
         <section className={style.contact} id='contact'>
             <div className='container'>
                 <h2 className='section-title'>Contact</h2>
-                <form name='contact' onSubmit={handleSubmit} data-netlify='true' netlify-honeypot='bot-field' className={style.form}>
+                <form name='contact' method='POST' onSubmit={handleSubmit} data-netlify='true' netlify-honeypot='bot-field' className={style.form}>
                     <input type='text' placeholder='Name' name='name' required />
                     <input type='email' placeholder='Email' name='email' required />
                     <textarea placeholder='Message' name='message' required />
